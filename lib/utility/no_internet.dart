f1359cda1b762bdd028f13503ab9ecc5b10bbb3f
Syak1234 <sayakmishra27@gmail.com>
2025-05-10 20:31:49 +0530

    update

--- lib/utility/no_internet.dart
+++ lib/utility/no_internet.dart
@@ -1,11 +1,10 @@
 
+
 import 'package:fitcoach/theme/app_colors.dart';
 import 'package:fitcoach/theme/font_Size.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
-
-
 class NoInternetScreen extends StatelessWidget {
   const NoInternetScreen({super.key});
 
